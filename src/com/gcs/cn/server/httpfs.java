fb4cfbe3e9aeedb283eba8614450ebb9ca695bd8
nilesh1168 <n.suryawanshi1168@gmail.com>
2023-11-05 11:04:32 -0500

    added a default value to the directory

--- src/com/gcs/cn/server/httpfs.java
+++ src/com/gcs/cn/server/httpfs.java
@@ -1,10 +1,11 @@
 package com.gcs.cn.server;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 public class httpfs {
 
-	public static String directory;
+	public static String directory = Paths.get("").toAbsolutePath().toString();
 
 	public static void main(String[] args) {
 		if(args.length == 1 && args[0].equals("help")){
